67d6890054ffaabf7d684c3593650b5eae36cb89
mahmoudeid <mahmoudeid2050@gmail.com>
2016-10-28 00:53:38 +0200

    Add project files

---
+++ js/quotes.js
@@ -0,0 +1,67 @@
+// quotes: array of object literals
+var quotes = [
+	{
+		quote: "You can do anything but not everything.",
+		source: "David Allen",
+		citation: "Making It All Work",
+		year: "2009",
+		tags: "business, success"
+	},
+	{
+		quote: "You miss 100 percent of the shots you never take.",
+		source: "Wayne Gretzky",
+		tags: "life, eyes"
+	},
+	{
+		quote: "Sometimes the questions are complicated and the answers are simple.",
+		source: "Dr. Seuss",
+		tags: "life"
+	},
+	{
+		quote: "You must be the change you wish to see in the world.",
+		source: "Gandhi",
+		tags: "life, success"
+	},
+	{
+		quote: "Great things in business are never done by one person. They're done by a team of people.",
+		source: "Steve Jobs",
+		tags: "business, success"
+	},
+	{
+		quote: "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+		source: "Albert Einstein",
+		tags: "life, simile"
+	},
+	{
+		quote: "Things change. And friends leave. Life doesn't stop for anybody.",
+		source: "Stephen Chbosky",
+		citation: "The Perks of Being a Wallflower",
+		tags: "change, life"
+	},
+	{
+		quote: "A successful man is one who makes more money than his wife can spend. A successful woman is one who can find such a man.",
+		source: "Lana Turner",
+		tags: "funny, money, woman, man"
+	},
+	{
+		quote: "If you can't make it good, at least make it look good.",
+		source: "Bill Gates",
+		tags: "business"
+	},
+	{
+		quote: "Change will not come if we wait for some other person or some other time. We are the ones we've been waiting for. We are the change that we seek.",
+		source: "Barack Obama",
+		tags: "life, success, change"
+	},
+	{
+		quote: "Try not to become a man of success, but rather try to become a man of value.",
+		source: "Albert Einstein",
+		tags: "man, value, try"
+	},
+	{
+		quote: "Behind every great man is a woman rolling her eyes.",
+		source: "Jim Carrey",
+		tags: "eyes, great, woman"
+	},
+
+];
---
+++ js/script.js
@@ -0,0 +1,102 @@
+/*
+* Author: Mahmoud Eid.
+* Description: Random quotes script.
+* Browsers the project was checked: Google Chrome, Mozilla Firefox, Safari.
+*/
+
+// event listener to respond to "Show another quote" button clicks.
+// when user clicks anywhere on the button, the "printQuote" function is called.
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+/*
+* This function used for get random quote.
+* Add displayCount property in every quote object.
+* Sort quotes ASC by displayCount.
+* And finally return random quote.
+*/
+function getRandomQuote() {
+	var randomNumber = Math.floor(Math.random() * quotes.length);
+	var randomQuote = quotes[randomNumber];
+	var viewsCounts = [];
+
+	// Add displayCount property in every quote object.
+	for (var i = 0; i < quotes.length; i += 1) {
+		if(!quotes[i].displayCount){
+			quotes[i].displayCount = 0;
+		}
+		viewsCounts.push(quotes[i].displayCount);
+	}
+	// sort views counts ASC
+	viewsCounts.sort(function(a, b){return a - b;});
+
+	// Loop through ASC sorted quotes
+	for (i = 0; i < quotes.length; i += 1) {
+		// if total of all quotes not equal 0, randomQuote variable equal first quote,
+		// which it's display count is less than other quotes.
+		if(quotes[i].displayCount === viewsCounts[0] && viewsCounts.reduce(getSum) !== 0) {
+			randomQuote = quotes[i];
+		}
+	}
+	return randomQuote;
+}
+
+/*
+* This function used for print Quote in web page.
+* Also increase quote display count.
+* Call refreshQoute function to return next Quote.
+* And finally call changePageBackground function.
+*/
+function printQuote() {
+	var quoteBox = document.getElementById("quote-box");
+	var randomQuote = getRandomQuote();
+	var quoteContent = '';
+
+	randomQuote.displayCount += 1 ;
+    quoteContent += '<p class="quote">' + randomQuote.quote +'</p>';
+    quoteContent += '<p class="source">' + randomQuote.source;
+    if(randomQuote.citation) {
+    	quoteContent += '<span class="citation">'+ randomQuote.citation +'</span>';
+    }
+    if(randomQuote.year) {
+    	quoteContent += '<span class="year">'+ randomQuote.year +'</span>';
+    }
+    quoteContent += '</p>';
+    if(randomQuote.tags){
+    	quoteContent += '<p class="tags">Tags: ' + randomQuote.tags +'</p>';
+    }
+    quoteBox.innerHTML = quoteContent;
+    
+    refreshQuote();
+    changePageBackground();
+}
+
+/*
+* This function used for auto refresh Quote.
+* By calling printQuote function every 30 Sec.
+*/
+function refreshQuote() {
+    setTimeout(function(){
+    	printQuote(); 
+    }, 30000);
+}
+
+/*
+* This function used for change page background.
+* By create random rgb color used for changing body and loadQuote button background.
+*/
+function changePageBackground() {
+	var color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+	document.body.style.backgroundColor = color;
+	document.getElementById("loadQuote").style.backgroundColor = color;
+}
+
+/*
+* Helper function.
+* sum of arrays.
+*/
+function getSum(total, num) {
+    return total + num;
+}
+
+// Call printQuote function.
+printQuote();
